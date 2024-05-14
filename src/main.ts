ad7f9654aabbbe7ddfcf1886e9fb5f7655779a9c
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-05-13 21:39:30 -0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import './style.css'
-import { setupCounter } from './counter.ts'
+const VANTA = window['VANTA'];
+VANTA.NET({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1,
+  scaleMobile: 1.00,
+  color: 'hsl(0, 100%, 74%)',
+  backgroundColor: 'hsl(220, 24%, 5%)'
+})
+let canvas = <HTMLElement>document.querySelector('.vanta-canvas');
+canvas.style.position = 'fixed';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/`
-  <nav>
-  </nav>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
