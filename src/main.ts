e9a32d47f07a659aca81d148e9c09a5b6d6b10dd
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-05-15 10:29:44 -0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,46 @@
-const VANTA = window['VANTA'];
+let scrollText = <NodeListOf<HTMLParagraphElement>>(
+  document.querySelectorAll('.ScrollContainer .scroll .RightToLeft p')
+);
+let skills = [
+  "Backend",
+  "Graphics Programming",
+  "Embedded",
+  "PCB Design",
+  "Frontend",
+  "GameDev",
+  "IOT",
+  "Creative Coding",
+  "Lighting Design",
+  "Blender",
+  "CAD",
+  "Visual Effects",
+  "Music Videos",
+  "Music Production",
+  "Shaders",
+];
+let skillStr = '';
+skills.forEach(skill => {
+  skillStr += skill + '    ';
+});
+scrollText.forEach((p) => {
+  p.style.whiteSpace = "pre";
+  p.textContent = skillStr;
+});
+
+
+const VANTA = window["VANTA"];
 VANTA.NET({
   el: "#background",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
+  minHeight: 200.0,
+  minWidth: 200.0,
   scale: 1,
-  scaleMobile: 1.00,
-  color: '#3DC7F5',
-  backgroundColor: 'hsl(220, 24%, 5%)'
-})
-let canvas = <HTMLElement>document.querySelector('.vanta-canvas');
-canvas.style.position = 'fixed';
-
+  scaleMobile: 1.0,
+  color: "#3DC7F5",
+  backgroundColor: "hsl(220, 24%, 5%)",
+});
+let canvas = <HTMLElement>document.querySelector(".vanta-canvas");
+canvas.style.position = "fixed";
 
